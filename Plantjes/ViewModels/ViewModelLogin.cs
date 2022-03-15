205098e8b4570f0861a0b131a8ec68a40d92e489
WarreBogaerts <warre.bogaerts@student.vives.be>
2022-03-15 14:37:58 +0100

    Red Boxes

--- Plantjes/ViewModels/ViewModelLogin.cs
+++ Plantjes/ViewModels/ViewModelLogin.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 using Microsoft.Toolkit.Mvvm.Input;
 using Plantjes.Models.Enums;
 using Plantjes.ViewModels.Interfaces;
@@ -7,14 +8,16 @@ using Plantjes.Views.Home;
 //written by kenny
 namespace Plantjes.ViewModels; 
 
-public class ViewModelLogin : ViewModelBase {
+public class ViewModelLogin : ViewModelBase 
+{
     private string _errorMessage;
     private string _loggedInMessage;
     public string _passwordInput;
 
     private string _userNameInput;
 
-    public ViewModelLogin(IloginUserService loginUserService) {
+    public ViewModelLogin(IloginUserService loginUserService) 
+    {
         _loginService = loginUserService;
         loginCommand = new RelayCommand(LoginButtonClick);
         cancelCommand = new RelayCommand(CancelButton);
@@ -26,13 +29,15 @@ public class ViewModelLogin : ViewModelBase {
     public RelayCommand cancelCommand { get; set; }
     public RelayCommand registerCommand { get; set; }
 
-    public string errorMessage {
+    public string errorMessage 
+    {
         get => _errorMessage;
         set {
             _errorMessage = value;
         }
     }
-    public string userNameInput {
+    public string userNameInput 
+    {
         get => _userNameInput;
         set {
             _userNameInput = value;
@@ -40,7 +45,8 @@ public class ViewModelLogin : ViewModelBase {
         }
     }
 
-    public string passwordInput {
+    public string passwordInput 
+    {
         get => _passwordInput;
         set {
             _passwordInput = value;
@@ -48,32 +54,69 @@ public class ViewModelLogin : ViewModelBase {
         }
     }
 
-    public void RegisterButtonView() {
+    public void RegisterButtonView() 
+    {
         var registerWindow = new RegisterWindow();
         registerWindow.Show();
         Application.Current.Windows[0]?.Close();
     }
 
-    public void CancelButton() {
+    public void CancelButton() 
+    {
         Application.Current.Shutdown();
     }
 
-    private void LoginButtonClick() {
-        if (!string.IsNullOrWhiteSpace(userNameInput)) {
+    private SolidColorBrush _GebruikersNaamColor;
+    public SolidColorBrush GebruikersNaamColor
+    {
+        get
+        {
+            return _GebruikersNaamColor;
+        }
+        set
+        {
+            _GebruikersNaamColor = value;
+            RaisePropertyChanged("GebruikersNaamColor");
+        }
+    }
+
+    private SolidColorBrush _PasswordColor;
+    public SolidColorBrush PasswordColor
+    {
+        get
+        {
+            return _PasswordColor;
+        }
+        set
+        {
+            _PasswordColor = value;
+            RaisePropertyChanged("PasswordColor");
+        }
+    }
+
+    private void LoginButtonClick() 
+    {
+        if (!string.IsNullOrWhiteSpace(userNameInput)) 
+        {
             var loginResult = _loginService.CheckCredentials(userNameInput, passwordInput);
 
-            if (loginResult.loginStatus == LoginStatus.LoggedIn) {
+            if (loginResult.loginStatus == LoginStatus.LoggedIn) 
+            {
                 //  loggedInMessage = _loginService.LoggedInMessage(userNameInput);
                 var mainWindow = new MainWindow();
                 mainWindow.Show();
                 Application.Current.Windows[0]?.Close();
             }
-            else {
+            else if (loginResult.loginStatus == LoginStatus.NotLoggedIn)
+            {
                 errorMessage = loginResult.errorMessage;
+                GebruikersNaamColor = new SolidColorBrush(Colors.Red);
             }
         }
-        else {
+        else 
+        {
             errorMessage = "Gebruikersnaam invullen.";
+            GebruikersNaamColor = new SolidColorBrush(Colors.Red);
         }
         RaisePropertyChanged("errorMessage");
     }
