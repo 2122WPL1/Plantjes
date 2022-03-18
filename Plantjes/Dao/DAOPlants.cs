da921a49fb402353cbd917cdea18106763495cd5
r0825266 <andang.kloranawah@student.vives.be>
2022-03-18 15:57:34 +0000

    DAOComentaar verwijderen van DAOPlants

--- Plantjes/Dao/DAOPlants.cs
+++ Plantjes/Dao/DAOPlants.cs
@@ -268,37 +268,13 @@ namespace Plantjes.Dao
             return selection;
         }
 
-        //public IQueryable<Fenotype> fillFenoTypeRatioBloeiBlad()
-        //{
-        //    // this is NOT part of the cascade function and wil not be added as it is not needed 
-        //    // request List of wanted type
-        //    // distinct to prevrent more than one of each type
-        //    // The if else is to check if something is selected in the previous combobox. if its not he doesn't filter.
-        //    // Here we use IQueryable<T>, it makes it easier for us to use our search queries and find the objects that we need.
-        //    // This will also make it possible for us to use all the properties instead of only a selection of an object in our ViewModels.
-        //    // Good way to interact with our datacontext
-
-        //    var selection = context.Fenotypes.Distinct().OrderBy(s => s.RatioBloeiBlad);
-        //    return selection;
-        //}
+        #endregion
 
         #region FilterFromPlant
 
         ///Owen: op basis van basiscode Kenny, Christophe
 
         #region FilterFenoTypeFromPlant
-
-        //public IQueryable<Fenotype> filterFenoTypeFromPlant(int selectedItem)
-        //{
-        //    var selection = context.Fenotypes.Distinct().Where(s => s.PlantId == selectedItem);
-        //    return selection;
-        //}
-
-        //public IQueryable<FenotypeMulti> FilterFenotypeMultiFromPlant(int selectedItem)
-        //{
-        //    var selection = context.FenotypeMultis.Distinct().Where(s => s.PlantId == selectedItem);
-        //    return selection;
-        //}
 
         #endregion
 
