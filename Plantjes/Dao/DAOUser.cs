0578cf97e2ad15010b88a00958db886490e107dd
The Arcane Brony <myrainbowdash949@gmail.com>
2022-04-28 15:19:53 +0200

    Add rnumber length check at register/import

--- Plantjes/Dao/DAOUser.cs
+++ Plantjes/Dao/DAOUser.cs
@@ -1,169 +1,171 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Plantjes.Models.Db;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
-using Plantjes.ViewModels;
+using Plantjes.Models.Db;
 
-namespace Plantjes.Dao {
-    //Gesplitst door Xander, aangepast door Warre
-    internal class DAOUser : DAOLogic {
-        public static Gebruiker GetGebruikerWithEmail(string userEmail) {
-            var x = context.Gebruikers.Include(x => x.Rol).SingleOrDefault(g => g.Emailadres == userEmail);
-            return x;
-        }
+namespace Plantjes.Dao; 
 
-        //written by kenny
-        public static void RegisterUser(string vivesNr, string firstName, string lastName, string emailadres, string password) {
-            var passwordBytes = Encoding.ASCII.GetBytes(password);
+//Gesplitst door Xander, aangepast door Warre
+internal class DAOUser : DAOLogic {
+    public static Gebruiker GetGebruikerWithEmail(string userEmail) {
+        var x = context.Gebruikers.Include(x => x.Rol).SingleOrDefault(g => g.Emailadres == userEmail);
+        return x;
+    }
+
+    //written by kenny
+    public static void RegisterUser(string vivesNr, string firstName, string lastName, string emailadres, string password) {
+        var passwordBytes = Encoding.ASCII.GetBytes(password);
+        var md5Hasher = new MD5CryptoServiceProvider();
+        var passwordHashed = md5Hasher.ComputeHash(passwordBytes);
+        Rol role;
+        if (emailadres.EndsWith("@vives.be")) role = context.Rols.First(x => x.Omschrijving == "Docent");
+        else if (emailadres.EndsWith("@student.vives.be")) role = context.Rols.First(x => x.Omschrijving == "Student");
+        else role = context.Rols.First(x => x.Omschrijving == "Oud-Student");
+        if (role == null) throw new MissingMemberException("Rol niet gevonden!");
+        var gebruiker = new Gebruiker {
+            Vivesnr = vivesNr,
+            Voornaam = firstName,
+            Achternaam = lastName,
+            Emailadres = emailadres,
+            HashPaswoord = passwordHashed,
+            Rol = role
+        };
+        context.Gebruikers.Add(gebruiker);
+        context.SaveChanges();
+    }
+
+    //written by kenny
+    public static List<Gebruiker> getAllGebruikers() {
+        return context.Gebruikers.ToList();
+    }
+
+    //written by kenny
+    public static bool GetEmailInUse(string email) {
+        return context.Gebruikers.Any(x => x.Emailadres == email);
+    }
+
+    //<Xander Baes>
+    /// <summary>
+    ///     Add users from CSV
+    /// </summary>
+    public static void AddUsersFromCsv() {
+        //import
+        importUsersFromCsv();
+        //show errors
+        openInExcelAndWait("errors.csv");
+    }
+
+    //legacy - object to csv column names
+    /*private static void generateCsv() {
+        //generate csv with headings
+        //delete if exists
+        if (File.Exists("import.csv")) File.Delete("import.csv");
+        //get fields
+        var fields = typeof(ViewModelRegister).GetProperties().Where(x => x.Name.Contains("Input")).Where(x => !x.Name.Contains("password")).Select(x => x.Name.Replace("Input", ""));
+        //write as column headings
+        File.WriteAllText("import.csv", String.Join(",", fields));
+    }*/
+
+    private static void openInExcelAndWait(string filename) {
+        //search for excel, or fall back to notepad
+        var executable = "notepad";
+        if (File.Exists(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"))
+            executable = @"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE";
+        else if (File.Exists(@"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE"))
+            executable = @"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE";
+        //execute
+        var proc = Process.Start(executable, filename);
+        //wait for exit, seem sto fire almost immediately
+        proc.WaitForExit();
+        //wait until file readable (unlocked) because Microsoft decided that you shouldn't be able to do this, for some reason
+        var ExcelExited = false;
+        while (!ExcelExited)
+            try {
+                File.ReadAllLines(filename);
+                ExcelExited = true;
+            }
+            catch { }
+    }
+
+    private static void importUsersFromCsv() {
+        //import from csv
+        //open files
+        if (File.Exists("errors.csv")) File.Delete("errors.csv");
+        var ofd = new OpenFileDialog {
+            Multiselect = false,
+            CheckPathExists = true,
+            CheckFileExists = true,
+            Title = "Selecteer CSV bestand met gebruikers.",
+            Filter = "Leerlingenlijst (*.csv)|*.csv",
+            FileName = "Excel KU-Loket -> save as .csv",
+            AddExtension = true,
+            DefaultExt = "*.csv",
+            ShowReadOnly = false
+        };
+
+        if ((bool)!ofd.ShowDialog()) return;
+        var ifs = File.OpenText(ofd.FileName);
+        var log = new List<string>();
+        //get fields
+        var fields = ifs.ReadLine().Split(",").ToList();
+        //get student role
+        var role = context.Rols.First(x => x.Omschrijving == "Student");
+        int errors = 0, added = 0;
+        //read all lines...
+        var line = "";
+        while ((line = ifs.ReadLine()) != null && line != "") {
+            //split by fields
+            var infields = line.Split(",");
+            //get password
+            var passwordBytes = Encoding.ASCII.GetBytes(infields[fields.ToList().IndexOf("Studentennummer")]);
             var md5Hasher = new MD5CryptoServiceProvider();
             var passwordHashed = md5Hasher.ComputeHash(passwordBytes);
-            Rol role;
-            if (emailadres.EndsWith("@vives.be")) role = context.Rols.First(x => x.Omschrijving == "Docent");
-            else if (emailadres.EndsWith("@student.vives.be")) role = context.Rols.First(x => x.Omschrijving == "Student");
-            else role = context.Rols.First(x => x.Omschrijving == "Oud-Student");
-            if (role == null) throw new MissingMemberException("Rol niet gevonden!");
-            var gebruiker = new Gebruiker {
-                Vivesnr = vivesNr,
-                Voornaam = firstName,
-                Achternaam = lastName,
-                Emailadres = emailadres,
+            //create user object
+            var user = new Gebruiker {
+                Vivesnr = infields[fields.IndexOf("Studentennummer")],
+                Voornaam = infields[fields.IndexOf("Voornaam")],
+                Achternaam = infields[fields.IndexOf("Familienaam")],
+                Emailadres = infields[fields.IndexOf("Emailadres")],
+                Rol = role,
                 HashPaswoord = passwordHashed,
-                Rol = role
+                LastLogin = DateTime.UnixEpoch
             };
-            context.Gebruikers.Add(gebruiker);
-            context.SaveChanges();
-        }
-
-        //written by kenny
-        public static List<Gebruiker> getAllGebruikers() {
-            return context.Gebruikers.ToList();
-        }
-
-        //written by kenny
-        public static bool GetEmailInUse(string email) {
-            return context.Gebruikers.Any(x => x.Emailadres == email);
-        }
-
-        //<Xander Baes>
-        /// <summary>
-        /// Add users from CSV
-        /// </summary>
-        public static void AddUsersFromCsv() {
-            //import
-            importUsersFromCsv();
-            //show errors
-            openInExcelAndWait("errors.csv");
-        }
-
-        //legacy - object to csv column names
-        /*private static void generateCsv() {
-            //generate csv with headings
-            //delete if exists
-            if (File.Exists("import.csv")) File.Delete("import.csv");
-            //get fields
-            var fields = typeof(ViewModelRegister).GetProperties().Where(x => x.Name.Contains("Input")).Where(x => !x.Name.Contains("password")).Select(x => x.Name.Replace("Input", ""));
-            //write as column headings
-            File.WriteAllText("import.csv", String.Join(",", fields));
-        }*/
-
-        private static void openInExcelAndWait(string filename) {
-            //search for excel, or fall back to notepad
-            string executable = "notepad";
-            if (File.Exists(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"))
-                executable = @"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE";
-            else if (File.Exists(@"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE"))
-                executable = @"C:\Program Files (x86)\Microsoft Office\root\Office16\EXCEL.EXE";
-            //execute
-            Process proc = Process.Start(executable, filename);
-            //wait for exit, seem sto fire almost immediately
-            proc.WaitForExit();
-            //wait until file readable (unlocked) because Microsoft decided that you shouldn't be able to do this, for some reason
-            bool ExcelExited = false;
-            while (!ExcelExited) {
-                try {
-                    File.ReadAllLines(filename);
-                    ExcelExited = true;
-                }
-                catch { }
+            //duplicate/data checks
+            if (context.Gebruikers.Any(x => x.Emailadres == user.Emailadres)) {
+                errors++;
+                log.Add($"Gebruiker met email {user.Emailadres} bestaat al!");
+            }
+            else if (context.Gebruikers.Any(x => x.Vivesnr == user.Vivesnr)) {
+                errors++;
+                log.Add($"Gebruiker met Vives-nummer {user.Vivesnr} bestaat al!");
+            }
+            else if (!user.Emailadres.Contains("@")) {
+                errors++;
+                log.Add($"Emailadres {user.Emailadres} is geen geldig emailadres!");
+            }
+            else if (user.Vivesnr.Length != 7) {
+                errors++;
+                log.Add($"Emailadres {user.Vivesnr} is geen geldig emailadres!");
+            }
+            else {
+                //all checks passed, add to db
+                context.Gebruikers.Add(user);
+                context.SaveChanges();
+                added++;
             }
         }
 
-        private static void importUsersFromCsv() {
-            //import from csv
-            //open files
-            if (File.Exists("errors.csv")) File.Delete("errors.csv");
-            var ofd = new OpenFileDialog() {
-                Multiselect = false,
-                CheckPathExists = true,
-                CheckFileExists = true,
-                Title = "Selecteer CSV bestand met gebruikers.",
-                Filter = "Leerlingenlijst (*.csv)|*.csv",
-                FileName = "Excel KU-Loket -> save as .csv",
-                AddExtension = true,
-                DefaultExt = "*.csv",
-                ShowReadOnly = false
-            };
-
-            if ((bool)!ofd.ShowDialog()) return;
-            var ifs = File.OpenText(ofd.FileName);
-            var log = new List<string>();
-            //get fields
-            var fields = ifs.ReadLine().Split(",").ToList();
-            //get student role
-            var role = context.Rols.First(x => x.Omschrijving == "Student");
-            int errors = 0, added = 0;
-            //read all lines...
-            string line = "";
-            while ((line = ifs.ReadLine()) != null && line != "") {
-                //split by fields
-                var infields = line.Split(",");
-                //get password
-                var passwordBytes = Encoding.ASCII.GetBytes(infields[fields.ToList().IndexOf("Studentennummer")]);
-                var md5Hasher = new MD5CryptoServiceProvider();
-                var passwordHashed = md5Hasher.ComputeHash(passwordBytes);
-                //create user object
-                var user = new Gebruiker() {
-                    Vivesnr = infields[fields.IndexOf("Studentennummer")],
-                    Voornaam = infields[fields.IndexOf("Voornaam")],
-                    Achternaam = infields[fields.IndexOf("Familienaam")],
-                    Emailadres = infields[fields.IndexOf("Emailadres")],
-                    Rol = role,
-                    HashPaswoord = passwordHashed,
-                    LastLogin = DateTime.UnixEpoch
-                };
-                //duplicate/data checks
-                if (context.Gebruikers.Any(x => x.Emailadres == user.Emailadres)) {
-                    errors++;
-                    log.Add($"Gebruiker met email {user.Emailadres} bestaat al!");
-                }
-                else if (context.Gebruikers.Any(x => x.Vivesnr == user.Vivesnr)) {
-                    errors++;
-                    log.Add($"Gebruiker met Vives-nummer {user.Vivesnr} bestaat al!");
-                }
-                else if (!user.Emailadres.Contains("@")) {
-                    errors++;
-                    log.Add($"Emailadres {user.Emailadres} is geen geldig emailadres!");
-                }
-                else {
-                    //all checks passed, add to db
-                    context.Gebruikers.Add(user);
-                    context.SaveChanges();
-                    added++;
-                }
-            }
-
-            //add status header
-            log.Insert(0, $"Added: {added},Errors:{errors}");
-            File.WriteAllLines("errors.csv", log);
-            //close files
-            ifs.Close();
-        }
-        //</Xander Baes>
+        //add status header
+        log.Insert(0, $"Added: {added},Errors:{errors}");
+        File.WriteAllLines("errors.csv", log);
+        //close files
+        ifs.Close();
     }
+    //</Xander Baes>
 }
--- Plantjes/ViewModels/Services/LoginUserService.cs
+++ Plantjes/ViewModels/Services/LoginUserService.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-
 using Plantjes.Dao;
 using Plantjes.Models.Classes;
 using Plantjes.Models.Db;
@@ -12,13 +11,11 @@ using Plantjes.Views.Home;
 
 namespace Plantjes.ViewModels.Services;
 
-public class LoginUserService : IloginUserService, INotifyPropertyChanged
-{
+public class LoginUserService : IloginUserService, INotifyPropertyChanged {
     //dao verklaren om data op te vragen en te setten in de databank
     private readonly DAOLogic _dao;
 
-    public LoginUserService()
-    {
+    public LoginUserService() {
         _dao = DAOLogic.Instance();
     } //gebruiker verklaren  om te gebruiken in de logica
 
@@ -26,22 +23,18 @@ public class LoginUserService : IloginUserService, INotifyPropertyChanged
 
     #region Register Region
 
-    public string RegisterButton(string vivesNrInput, string lastNameInput, string firstNameInput,
-        string emailAdresInput, string passwordInput, string passwordRepeatInput)
-    {
+    public string RegisterButton(string vivesNrInput, string lastNameInput, string firstNameInput, string emailAdresInput, string passwordInput, string passwordRepeatInput) {
         //errorMessage die gereturned wordt om de gebruiker te waarschuwen wat er aan de hand is
         var Message = string.Empty;
         //checken of alle velden ingevuld zijn
-        if (vivesNrInput != null && firstNameInput != null && lastNameInput != null && emailAdresInput != null && passwordInput != null && passwordRepeatInput != null)
-        {
+        if (vivesNrInput != null && firstNameInput != null && lastNameInput != null && emailAdresInput != null && passwordInput != null && passwordRepeatInput != null) {
             //checken als het emailadres een geldig vives email is.
             if (emailAdresInput != null && emailAdresInput.Contains(".") && emailAdresInput.Contains("@")
                 //checken als het email adres al bestaat of niet.
-                && !DAOUser.GetEmailInUse(emailAdresInput))
-            {
+                && !DAOUser.GetEmailInUse(emailAdresInput)) {
+                if ((vivesNrInput != null) & (vivesNrInput.Length != 8)) return "Dit is geen geldig vives nummer!";
                 //checken als het herhaalde wachtwoord klopt of niet.
-                if (passwordInput == passwordRepeatInput)
-                {
+                if (passwordInput == passwordRepeatInput) {
                     //gebruiker registreren.
                     DAOUser.RegisterUser(vivesNrInput, firstNameInput, lastNameInput, emailAdresInput, passwordInput);
                     //Message = $"{firstNameInput}, je bent succevol geregistreerd,"+"\r\n"+$" uw gebruikersnaam is {emailAdresInput}." + 
@@ -75,8 +68,7 @@ public class LoginUserService : IloginUserService, INotifyPropertyChanged
     public event PropertyChangedEventHandler PropertyChanged;
 
     //het eigenlijke loginsysteem
-    public LoginResult CheckCredentials(string userNameInput, string passwordInput)
-    {
+    public LoginResult CheckCredentials(string userNameInput, string passwordInput) {
         //Nieuw loginResult om te gebruiken, status op NotLoggedIn zetten
         var loginResult = new LoginResult { loginStatus = LoginStatus.NotLoggedIn };
 
@@ -87,23 +79,20 @@ public class LoginUserService : IloginUserService, INotifyPropertyChanged
             gebruiker = DAOUser.GetGebruikerWithEmail(userNameInput);
             loginResult.gebruiker = gebruiker;
         }
-        else
-        {
+        else {
             //indien geen geldig emailadress, errorMessage opvullen
             loginResult.errorMessage = "FOUT! Dit is geen geldig Vives emailadres.";
             return loginResult;
         }
 
         //xander - password check
-        if (passwordInput != null)
-        {
+        if (passwordInput != null) {
             //omzetten van het ingegeven passwoord naar een gehashed passwoord
             var passwordBytes = Encoding.ASCII.GetBytes(passwordInput);
             var md5Hasher = new MD5CryptoServiceProvider();
             var passwordHashed = md5Hasher.ComputeHash(passwordBytes);
 
-            if (gebruiker != null)
-            {
+            if (gebruiker != null) {
                 _gebruiker = gebruiker;
                 loginResult.gebruiker = gebruiker;
                 //passwoord controle
@@ -114,25 +103,23 @@ public class LoginUserService : IloginUserService, INotifyPropertyChanged
                     //indien false errorMessage opvullen
                     loginResult.errorMessage += "\r\n" + "FOUT! Het ingegeven wachtwoord is niet juist. Gelieve opnieuw te proberen.";
             }
-            else
-            {
+            else {
                 // als de gebruiker niet gevonden wordt, errorMessage invullen
                 loginResult.errorMessage = $"FOUT! Er is geen account gevonden voor {userNameInput}" + "\r\n" + "Gelieve eerst te registreren.";
             }
         }
-        else
-        {
+        else {
             //xander - password check
             loginResult.errorMessage = "Gelieve een wachtwoord in te geven.";
         }
 
         return loginResult;
     }
+
     //Functie om naam weer te geven in loginWindow, als login succesvol is
     public string LoggedInMessage() {
         var message = string.Empty;
-        if (_gebruiker != null)
-        {
+        if (_gebruiker != null) {
             message = $"Ingelogd als: {_gebruiker.Voornaam} {_gebruiker.Achternaam}";
             return message;
         }
