098ed19951e68abcadb8b2f8f9b9a2bbcbfd7a60
The Arcane Brony <myrainbowdash949@gmail.com>
2022-05-04 15:28:18 +0200

    Add basic bindings

---
+++ Plantjes/ModelSupport/Gebruiker.cs
@@ -0,0 +1,8 @@
+// ReSharper disable once CheckNamespace
+namespace Plantjes.Models.Db
+{
+    public partial class Gebruiker {
+        public string DisplayName => Achternaam + " " + Voornaam;
+        public string DisplayNameWithNr => $"{DisplayName} ({Vivesnr})";
+    }
+}
--- Plantjes/ViewModels/ViewModelUserManagement.cs
+++ Plantjes/ViewModels/ViewModelUserManagement.cs
@@ -1,10 +1,44 @@
-﻿using Microsoft.Toolkit.Mvvm.Input;
+﻿using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using Plantjes.Dao;
+using Plantjes.Models.Db;
 
 namespace Plantjes.ViewModels
 {
-    internal class ViewModelUserManagement: ViewModelBase
-    {
+    internal class ViewModelUserManagement: ViewModelBase {
+        private static ViewModelUserManagement instance;
+        private static Gebruiker _currentUser;
+        
+        public ViewModelUserManagement() {
+            instance = this;
+            DAOLogic.context.ChangeTracker.StateChanged += (sender, args) => {
+                args.NewState.PrintDebug();
+                
+            };
+            users.CollectionChanged += (sender, args) => {
+                foreach (var argsNewItem in args.NewItems!) {
+                    argsNewItem.PrintDebug();
+                }
+            };
+        }
+        
+        public ObservableCollection<Gebruiker> users { get => new(DAOLogic.context.Gebruikers);  }
+
+        public Gebruiker currentUser {
+            get => _currentUser;
+            set {
+                _currentUser = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+        public RelayCommand DeleteCommand { get; } = new(()=> {
+            DAOLogic.context.Gebruikers.Remove(_currentUser);
+            DAOLogic.context.SaveChanges();
+            instance.OnPropertyChanged("users");
+        });
         public RelayCommand ImportCsvCommand { get; } = new(DAOUser.AddUsersFromCsv);
+        
     }
 }
