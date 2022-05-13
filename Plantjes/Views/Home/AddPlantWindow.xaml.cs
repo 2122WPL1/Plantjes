d6b50c20f3dac63ee2337eca553ea113ef465f9d
Kjell Coussens <r0879467@student.vives.be>
2022-05-13 09:43:40 +0200

    User Controls fixes

--- Plantjes/Views/Home/AddPlantWindow.xaml.cs
+++ Plantjes/Views/Home/AddPlantWindow.xaml.cs
@@ -1,17 +1,26 @@
-﻿using Plantjes.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Plantjes.Views.Home
 {
     /// <summary>
-    /// Interaction logic for AddPlantWindow.xaml 
-    /// Written by Andang Kloran
+    /// Interaction logic for AddPlantWindow.xaml
     /// </summary>
     public partial class AddPlantWindow : Window
     {
         public AddPlantWindow()
         {
-            DataContext = App.Current.Services.GetService(typeof(ViewModelAddPlant));
             InitializeComponent();
         }
     }
